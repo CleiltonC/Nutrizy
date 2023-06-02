305d0a58ded23cb408f7d76fead1f0d2e1fb948f
Cleilton <cleiltoncs@hotmail.com>
2023-06-02 15:20:30 -0300

    Initial Commit

---
+++ Nutrizy/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Nutrizy
+//
+//  Created by fccs on 02/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Nutrizy/NutrizyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutrizyApp.swift
+//  Nutrizy
+//
+//  Created by fccs on 02/06/23.
+//
+
+import SwiftUI
+
+@main
+struct NutrizyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
