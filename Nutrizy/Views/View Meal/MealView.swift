13678805a8f4f20fbd7c73f52057d51c7a4f84c1
Daniel Araujo <danielaraujonobre14@gmail.com>
2023-06-06 15:28:30 -0300

    initial Commit Meal View

---
+++ Nutrizy/Views/View Meal/MealView.swift
@@ -0,0 +1,29 @@
+//
+//  SwiftUIView.swift
+//  Nutrizy
+//
+//  Created by user on 06/06/23.
+//
+
+import SwiftUI
+
+struct MealView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MealView_Previews: PreviewProvider {
+    static var previews: some View {
+        MealView()
+    }
+}
+
+struct CardMeal: View{
+    var body: some View{
+        HStack{
+//            Image()
+//            Text()
+        }
+    }
+}
