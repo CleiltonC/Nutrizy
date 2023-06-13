2ccdc852526cf72e5a8ad2b9b587075260943445
Vinicius Justa <viniciusxime@gmail.com>
2023-06-13 16:51:10 -0300

    Refactored CardFavorite

--- Nutrizy/Views/CardFavorite.swift
+++ Nutrizy/Views/CardFavorite.swift
@@ -10,10 +10,26 @@ import SwiftUI
 struct CardFavorite: View {
     var image: String
     var body: some View {
+        
         VStack {
-            Image(image)
-                .resizable()
-                .frame(width: 100, height: 100)
+            HStack(spacing: 10) {
+                Spacer()
+                Image(image)
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                
+                VStack(alignment: .leading, spacing: 5) {
+                    Text("Nome do prato")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    Text("Massa acompanhada de uma mistura de legumes com molho shoyo baseada na cultura asiática")
+                        .font(.body)
+                        .fontWeight(.thin)
+                        .frame(maxWidth: 240, alignment: .leading)
+                }.frame(maxHeight: 100)
+            }
         }
     }
 }
